04ed1aa324ea6afb56981f36b7df004b1707f86c
SempervirensSoftware <peter.terrill@gmail.com>
2014-10-14 00:13:20 -0700

    Adding Payment status codes

--- lib/stripe/payment.rb
+++ lib/stripe/payment.rb
@@ -1,5 +1,10 @@
 module Stripe
   class Payment < APIResource
+
+	STATUS = {
+		:pending => 'pending',
+		:paid => 'paid',
+	}
     
 	include Stripe::APIOperations::Create
 	include Stripe::APIOperations::List
