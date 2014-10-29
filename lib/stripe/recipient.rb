1972a14f0bd274d8652fd69fdd1dc89a3c26bfef
Connor Mullen <connormullen3@gmail.com>
2014-10-29 13:04:36 -0700

    add default_bank + default_card getters to customer + recipient

--- lib/stripe/recipient.rb
+++ lib/stripe/recipient.rb
@@ -8,5 +8,27 @@ module Stripe
     def transfers
       Transfer.all({ :recipient => id }, @api_key)
     end
+    
+    def get_default_card
+      response = nil
+
+      if (self.respond_to?(:default_card))
+        card_id = self.default_card
+        response = self.cards.data.select{ |card| card.id == card_id }.first
+      end
+
+      return response
+    end
+
+    def get_default_bank_account
+      response = nil
+
+      if (self.respond_to?(:default_bank_account))
+        bank_account_id = self.default_bank_account
+        response = self.bank_accounts.data.select{ |account| account.id == bank_account_id }.first
+      end
+
+      return response
+    end
   end
 end
